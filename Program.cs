d9c98aac45da9a224ad2ce48f07af6cb7d5f0534
ahhko <117479353+ahhko@users.noreply.github.com>
2022-11-22 12:14:52 +0300

    Revert "Revert "Автопарк""
    This reverts commit fccf591ca808e12240ba5c9d89aa54651134ef8c.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PassengerCar passengerCar1 =
+                new PassengerCar("Ford", 200, 2008, 9, new Dictionary<string, int>() { });
+            PassengerCar passengerCar2 =
+                new PassengerCar("Сitroen", 250, 2018, 8, new Dictionary<string, int>() { });
+            Truck truck1 = 
+                new Truck("Mercedez", 500, 2010, 3000, "Фомин Глеб", new Dictionary<string, int>());
+            Truck truck2 = 
+                new Truck("КамАз", 600, 2020, 4000, "Фролов Егор", new Dictionary<string, int>());
+
+            Autopark autopark;
+            autopark = new Autopark("My Autopark", new List<Car>() {passengerCar1, passengerCar2, truck1, truck2});
+            autopark.ToString();
+        }
+    }
+    
+}
