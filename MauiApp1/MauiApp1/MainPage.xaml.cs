a4d57040cc647206af758186ed4de17b9022e78f
ahhko <117479353+ahhko@users.noreply.github.com>
2022-12-09 18:18:57 +0300

    Практика MAUI
    Co-Authored-By: GalteXx <65533265+GalteXx@users.noreply.github.com>

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	string _PIN = "1234";
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void DigitClicked(object sender, EventArgs e)
+	{
+		if (DisplayLabel.Text != "PIN введен верно!")
+			DisplayLabel.Text += (sender as Button).Text;
+	}
+
+    private void ClearClicked(object sender, EventArgs e)
+    {
+        if (DisplayLabel.Text != "PIN введен верно!")
+            DisplayLabel.Text = "";
+    }
+
+    private void CheckClicked(object sender, EventArgs e)
+    {
+        if (DisplayLabel.Text != "PIN введен верно!")
+            if (DisplayLabel.Text == _PIN)
+			{
+				DisplayLabel.Text = "PIN введен верно!";
+			}
+			else DisplayLabel.Text = "";
+    }
+}
+
